d1d56104b5a5e96fde5beb01fb0f74a79ef68f28
Gonzalo lledo <gonzalo_lledo@outlook.com>
2020-03-28 19:26:58 +0100

    se sube data?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Data from './Data'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      confirmeddeaths: null,
+      confirmed: null,
+      recovered: null ,
+      loading: true
+    }
+
+  }
+
+  async componentDidMount () {
+
+    try{
+
+      const response = await fetch('https://enrichman.github.io/covid19/world/full.json')
+      const data = await response.json()
+
+      this.setState({
+        deaths: data.deaths,
+        confirmed: data.confirmed,
+        recovered: data.recovered,
+        loading: false
+      })
+
+    } catch (e){
+      console.log(e)
+    }
+  }
+
+  render(){
+    
+    return(
+      this.state.loading ?
+     
+      <div>Cargando los datos de hoy ...</div> 
+
+      :
+      
+      <Data deaths={this.state.deaths} confirmed={this.state.confirmed} recovered={this.state.recovered}></Data>
+    )
+    
+  }
 }
 
 export default App;
